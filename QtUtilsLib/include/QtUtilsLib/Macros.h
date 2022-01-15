98b3a00f0286be27a506829959fdcdc0358cf0a0
Daniel Borchart <daniel.borchart@protonmail.com>
2022-01-15 22:26:40 +0100

    added missing log macros

--- QtUtilsLib/include/QtUtilsLib/Macros.h
+++ QtUtilsLib/include/QtUtilsLib/Macros.h
@@ -2,7 +2,15 @@
 
 #include <utilsLib/Utils.h>
 
+#ifdef _DEBUG
+#define UTILS_QLOG_TRACE(msg) UTILS_LOG_TRACE(std::string(msg.toLocal8Bit().constData()))
 #define UTILS_QLOG_DEBUG(msg) UTILS_LOG_DEBUG(std::string(msg.toLocal8Bit().constData()))
+#else
+#define UTILS_QLOG_TRACE(msg)
+#define UTILS_QLOG_DEBUG(msg)
+#endif
+
 #define UTILS_QLOG_INFO(msg) UTILS_LOG_INFO(std::string(msg.toLocal8Bit().constData()))
 #define UTILS_QLOG_WARN(msg) UTILS_LOG_WARN(std::string(msg.toLocal8Bit().constData()))
+#define UTILS_QLOG_ERROR(msg) UTILS_LOG_ERROR(std::string(msg.toLocal8Bit().constData()))
 #define UTILS_QLOG_FATAL(msg) UTILS_LOG_FATAL(std::string(msg.toLocal8Bit().constData()))
